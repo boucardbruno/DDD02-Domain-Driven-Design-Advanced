598d5ddf68a4561cc47df6e47f6f7e5416caa95c
Bruno Boucard <boucard.bruno@free.fr>
2024-12-06 15:41:18 +0100

    Clean up

---
+++ TheaterSuggestions/Java/SeatsSuggestions/src/main/java/com/octo/technology/SeatsSuggestions/SuggestionNotAvailable.java
@@ -0,0 +1,9 @@
+package com.octo.technology.SeatsSuggestions;
+
+public class SuggestionNotAvailable extends SuggestionsMade {
+
+
+    public SuggestionNotAvailable(String showId, int partyRequested) {
+        super(showId, partyRequested);
+    }
+}
