4260d37aa59b93961dcca5059b79fdea2b2deef7
Bruno Boucard <boucard.bruno@free.fr>
2024-12-07 06:33:15 +0100

    .NET Migration .NET 8.0

--- TheaterSuggestions/CSharp/SeatsSuggestions.Api/Startup.cs
+++ TheaterSuggestions/CSharp/SeatsSuggestions.Api/Startup.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ExternalDependencies;
-using ExternalDependencies.AuditoriumLayoutRepository;
-using ExternalDependencies.ReservationsProvider;
+﻿using ExternalDependencies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace SeatsSuggestions.Api
 {
@@ -36,8 +27,8 @@ namespace SeatsSuggestions.Api
             IProvideCurrentReservations seatReservationsProvider = new SeatReservationsWebAdapter("http://localhost:5000/"); 
             // data_for_reservation_seats/
 
-            services.AddSingleton<IProvideAuditoriumLayouts>(auditoriumSeatingRepository);
-            services.AddSingleton<IProvideCurrentReservations>(seatReservationsProvider);
+            services.AddSingleton(auditoriumSeatingRepository);
+            services.AddSingleton(seatReservationsProvider);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
